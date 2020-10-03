85bfb487099432491dfa5c842a74e17844768b31
Max <max@maxf.net>
2020-10-03 14:01:44 -0400

    Build

---
+++ bin/run.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -e
+
+pushd "$(dirname $0)"
+SWD=$(pwd)
+BWD=$(dirname "$SWD")
+
+. $SWD/setenv.sh
+
+RUN_IMAGE="$REPO/$NAME"
+
+# Publish exposed ports
+imageId=$(docker images --format="{{.Repository}} {{.ID}}"|grep "^$RUN_IMAGE "|awk '{ print $2 }')
+while read port; do
+	hostPort=$DOCKER_PORT_PREFIX${port%%/*}
+	[ ${#hostPort} -gt 5 ] && hostPort=${hostPort:${#hostPort}-5}
+	DOCKER_RUN_ARGS+=( -p $hostPort:$port )
+done < <(docker image inspect -f '{{json .Config.ExposedPorts}}' $imageId|jq -r 'keys[]')
+
+DOCKER_RUN_ARGS=( -e container=docker )
+#DOCKER_RUN_ARGS+=( -v $BWD/mnt/etc/bind/named.conf:/etc/bind/named.conf )
+#DOCKER_RUN_ARGS+=( -v $BWD/mnt/etc/bind/named.conf.options:/etc/bind/named.conf.options )
+#DOCKER_RUN_ARGS+=( -v $BWD/mnt/etc/bind/named.conf.zones:/etc/bind/named.conf.zones )
+#DOCKER_RUN_ARGS+=( -v $BWD/mnt/etc/bind/zones:/var/bind/zones )
+
+docker stop $NAME || true
+docker system prune -f
+docker run -d -it "${DOCKER_RUN_ARGS[@]}" --name $NAME $RUN_IMAGE:$VERSION $*
+
+echo "Attaching to container. To detach CTRL-P CTRL-Q."
+docker attach $NAME
