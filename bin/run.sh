f5ab2a5c220fa16ff3a830ac1a3ed04b344a491f
Max <max@maxf.net>
2020-10-03 20:47:06 -0400

    Prevent accidental checking of sensitive data

--- bin/run.sh
+++ bin/run.sh
@@ -20,7 +20,7 @@ DOCKER_RUN_ARGS=( -e container=docker )
 
 DOCKER_RUN_ARGS+=( -v $BWD/mnt/etc/smtpd:/etc/smtpd )
 DOCKER_RUN_ARGS+=( -v $BWD/mnt/var/mail:/var/mail )
-DOCKER_RUN_ARGS+=( -v $BWD/mnt/etc/dkimproxy/dkimproxy_out.conf:/etc/dkimproxy/dkimproxy_out.conf )
+DOCKER_RUN_ARGS+=( -v $BWD/mnt/etc/dkimproxy:/etc/dkimproxy )
 
 docker stop $NAME || true
 docker system prune -f
--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -4,5 +4,6 @@ ls /var/mail | while read user; do
         adduser -D -H -s /bin/false $user
 done
 
+dkimproxy.out --conf_file=/etc/dkimproxy/dkimproxy_out.conf --daemonize 
 smtpd -dv
 
